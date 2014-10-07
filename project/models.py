6071fba004d6200752999a20c92b36ecff161751
Jonathan <jonathan.pastor@me.com>
2014-10-07 22:22:00 +0200

    add a complex model with required admin stuffs;

--- project/models.py
+++ project/models.py
@@ -4,18 +4,20 @@ from django.db import models
 class Match(models.Model):
     place = models.CharField(max_length=200)
     date = models.DateField(auto_now=True)
-    winer = models.ForeignKey('Player', related_name="win", default=None)
-    loser = models.ForeignKey('Player', related_name="lose", default=None)
 
     def __unicode__(self):
-        return self.winer.name + " VS " + self.loser.name
+        return self.place
 
 # Player class
 class Player(models.Model):
     name = models.CharField(max_length=200)
     age = models.IntegerField(default=0)
-    matchs = models.ManyToManyField(Match , blank=True)
     
     def __unicode__(self):
         return self.name
+
+class Participation(models.Model):
+    score = models.IntegerField(default=0)
+    player = models.ForeignKey(Player)
+    match = models.ForeignKey(Match)
 
